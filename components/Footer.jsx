60e079dd568efcddfb3a0cc830ba7ca627e45e8a
Shivang <125237608+Shivang-Rawat@users.noreply.github.com>
2023-08-26 15:06:36 +0530

    start

---
+++ components/Footer.jsx
@@ -0,0 +1,63 @@
+import { BsFacebook, BsYoutube } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { GoLocation } from "react-icons/go";
+import { FiPhone } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
+import Link from "next/link";
+
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-[#2F3F93] mt-10 text-center ">
+      <div className="block sm:flex justify-around p-16">
+
+        <div className="flex flex-col gap-4 lg:w-80 justify-center text-center items-center pt-5  ">
+          <img className="w-24 flex items-center justify-center" src="https://cdn.discordapp.com/attachments/1078905801017659432/1110839909721317386/image_1.png" alt=""/>
+          <div className="text-3xl text-white flex gap-10 justify-center items-center mt-8">
+            <Link href="https://www.facebook.com/edumynation" target="_blank"><BsFacebook /></Link>
+            <Link href="https://www.instagram.com/edumynation/" target="_blank"><BsInstagram /></Link>
+            <Link href="https://www.linkedin.com/company/edu-my-nation/about/" target="_blank"><BsLinkedin /></Link>
+            <Link href={'/'} ><BsYoutube /></Link>
+            <Link href="https://twitter.com/edumynation" target="_blank">{" "}
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="white" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+            </Link>
+          </div>
+        </div>
+
+        <div className="px-14 ">
+          <div><p className="text-white text-[30px]  font-5xl font-semibold mt-4  mb-4 text-left"> Contact</p></div>
+            <div className=" flex items-center gap-3 mb-4 text-[14px]">
+              <GoLocation  className="text-xl"/><p>903, Saeed Tower-1, Sheikh Zayed Road, Dubai, UAE</p>
+            </div>
+            <div className="flex items-center gap-3 mb-4  text-[14px] ">
+              <FiPhone className="text-xl" /><Link href="tel:+971 56 822 8740" target="_blank">+971 52 102 6963</Link>
+            </div>
+            <div className="flex items-center gap-3 mb-4 text-[14px] ">
+              <HiOutlineMail className="text-xl" /><Link href="mailto:info@edumynation.com">info@edumynation.com</Link>
+            </div>
+       </div>
+
+        <div className="text-white text-[16px] gap-4 hidden sm:block">
+           <div><p className="text-white text-[30px]  font-3xl font-semibold my-4 text-left"> Important Links</p></div>
+           <div className="flex  gap-10 text-left" > 
+           <div className="flex flex-col space-y-4">
+            <Link href={'/'} >Home</Link>
+            <Link href={'/'} >Privacy Policies</Link>
+            <Link href={'/'} >About</Link>
+            </div>
+            <div  className="flex flex-col space-y-4">
+            <Link href={"/"}>Terms & Condition</Link>
+            <Link href={'/'} >Blogs</Link>
+            <Link href={'/'} >SiteMap</Link>
+            </div>
+          </div></div>
+      </div>
+      <div className="text-center text-white pb-4"><p>copyright@edumynation</p></div>
+
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from 'react'
+import { useState } from 'react';
+import { Drawer, Typography, IconButton } from '@material-tailwind/react';
+import { BsTelephone, BsWhatsapp } from 'react-icons/bs';
+import {GiCancel} from 'react-icons/gi';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () =>  {
+  const [navbar, setNavbar] = useState(false);
+  const [openRight, setOpenRight] = useState(false);
+  const openDrawerRight = () => setOpenRight(true);
+  const closeDrawerRight = () => setOpenRight(false);
+
+  return (
+
+    <div>
+      <nav className=" w-full absolute top-0 z-50 ">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 ">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-7 md:block">
+              <Link href={'/'} ><img className="w-14 lg:w-22" src={"https://cdn.discordapp.com/attachments/1078905801017659432/1110839909721317386/image_1.png"} alt=""  width={88} height={56} /></Link>
+              <div className="md:hidden flex gap-5 text-white">
+                <Link href="whatsapp://send?abid=+971568228740"><BsWhatsapp className="inline w-6 h-6" /></Link>
+                <Link href="tel:+971568228740"><BsTelephone className="inline w-6 h-6" /></Link>
+                <button className="text-gray-700 rounded-md outline-none focus:border-gray-900 focus:border" onClick={openDrawerRight}>
+                  {navbar ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" className=" text-white"/>
+                    </svg>
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" className=" text-white"/>
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={`flex-1 justify-self-center pb-3 mt-1 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden' }`}>
+              <ul className="font-medium items-center w-full opacity-90 text-center justify-center space-y-4 md:flex md:space-x-12 md:space-y-0 py-10 md:py-0">
+                <li className="text-white hover:text-[#6366F1]"><Link href="/" >Home</Link></li>
+                <li className="text-white hover:text-[#6366F1]"><Link href="/about-us" >About Us</Link></li>
+                <li className="text-white hover:text-[#6366F1]"><Link href="/contact-us" >Contact Us</Link></li>
+                <li className="text-white hover:text-[#6366F1]"><Link href="https://blog.edumynation.com/"  target="_blank" className="font-medium">Blogs</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      {/* <React.Fragment>
+        <Drawer placement="right" open={openRight} onClose={closeDrawerRight} className="p-4 bg-[#2F3F93]">
+          <div className="mb-6 flex items-center justify-between">
+            <Typography variant="h5" color="indigo"></Typography>
+            <IconButton variant="text" color="white" onClick={closeDrawerRight}><GiCancel strokeWidth={2} className="h-5 w-5" /></IconButton>
+          </div>
+          <div className="flex flex-col text-white gap-5">
+            <span onClick={() => btnClick('/')}>Home</span>
+            <span onClick={() => btnClick('/')}>About</span>
+            <span onClick={() => btnClick('/')}>Contact Us</span>
+            <span><Link href="https://blog.edumynation.com/" target="_blank" className="font-medium">Blogs</Link></span>
+            <Link href={'/'} >Privacy Policies</Link>
+            <Link href={'/'} >Terms & Conditions</Link>
+            <Link href={'/'} >Registered School</Link>
+            <Link href={'/'} >Terms & Conditions For School</Link>
+          </div>
+        </Drawer>
+      </React.Fragment> */}
+    </div>
+  );
+}
+
+
+export default Navbar;
