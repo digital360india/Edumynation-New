3aedd55d9ef33bd726feae906239f14a796f89db
Nakul Thakur <thakurnakul119@gmail.com>
2023-10-04 14:49:02 +0530

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,12 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: "Edumynation",
   description: "UAE's premier school listing portal, offering comprehensive insights into the  educational landscape. Compare and select the ideal school for your child.",
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 };
 
 const RootLayout = ({ children }) => (
   <html lang='en'>
     <head>
-      <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1078905801017659432/1110839909721317386/image_1.png" type="image/x-icon" />
+      {/* <link rel="icon" href="https://cdn.discordapp.com/attachments/1078905801017659432/1110839909721317386/image_1.png" type="image/x-icon" sizes="any" /> */}
     </head>
     <body style={{backgroundColor:"white"}}>
         <main className='app  '>
