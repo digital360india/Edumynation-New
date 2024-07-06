36dc4dc9aff76141e98c1b125704a5f231435ba3
Himanshu Verma <himanshuverma544@gmail.com>
2024-07-06 10:33:15 +0530

    upd: added website theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    // extend: {
-    //   backgroundImage: {
-    //     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-    //     'gradient-conic':
-    //       'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-    //   },
-    // },
+    extend: {
+      colors: {
+        primaryBlue: '#2F3F93',
+        primaryLight: '#F8F4F1',
+        secondaryLight: "#F9F9F9"
+      },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+    },
   },
   plugins: [
-    require('tailwind-scrollbar-hide')
+    require('tailwind-scrollbar-hide'),
   ],
 }
