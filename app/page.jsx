25955e9aa80ef95631d25b27b74dbf6496e76151
Himanshu Verma <himanshuverma544@gmail.com>
2024-07-06 10:37:07 +0530

    upd: imported sections and using them

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,21 @@
 'use client';
 
 import Hero from '../components/pages/home/hero-section/Hero';
+import Resources from '../components/pages/home/resources-section/Resources';
+import SchoolByCities from '../components/pages/home/school-by-cities-section/SchoolByCities';
+import StudentServices from '../components/pages/home/student-services-section/StudentServices';
+import FeaturedSchools from '../components/pages/home/featured-schools-section/FeaturedSchools';
 
 
 export default function Home() {
 
-
   return (
     <>
       <Hero/>
+      <Resources/>
+      <SchoolByCities/>
+      <StudentServices/>
+      <FeaturedSchools/>
     </>
   );
 }
