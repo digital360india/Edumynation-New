c3779726657ca3082a99809d3157ea0ad67a70a1
Himanshu Verma <himanshuverma544@gmail.com>
2024-07-08 10:56:10 +0530

    upd: imported the section components and used them fo rendering

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,9 @@ import Resources from '../components/pages/home/resources-section/Resources';
 import SchoolByCities from '../components/pages/home/school-by-cities-section/SchoolByCities';
 import StudentServices from '../components/pages/home/student-services-section/StudentServices';
 import FeaturedSchools from '../components/pages/home/featured-schools-section/FeaturedSchools';
+import AboutUs from '../components/pages/home/about-us-section/AboutUs';
+import Testimonials from '../components/pages/home/testimonials-section/Testimonials';
+import ContactUsForm from '../components/pages/home/contact-us-form-section/ContactUsForm';
 
 
 export default function Home() {
@@ -16,6 +19,9 @@ export default function Home() {
       <SchoolByCities/>
       <StudentServices/>
       <FeaturedSchools/>
+      <AboutUs/>
+      <Testimonials/>
+      <ContactUsForm/>
     </>
   );
 }
