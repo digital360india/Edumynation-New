311fd66ef6d572c2d4896bd589f184f587826bbd
Himanshu Verma <himanshuverma544@gmail.com>
2024-07-09 10:32:48 +0530

    upd: made featured schools section responsive

--- components/pages/home/featured-schools-section/FeaturedSchools.jsx
+++ components/pages/home/featured-schools-section/FeaturedSchools.jsx
@@ -51,14 +51,14 @@ export default function FeaturedSchools() {
 
   return (
     <section id="featured-schools" className="flex flex-col gap-10 px-5 py-5 bg-primaryLight lg:px-7 xl:px-9">
-      <h2 className="px-2 text-3xl font-semibold text-primaryBlue">
+      <h2 className="text-3xl font-semibold text-primaryBlue">
         Featured Schools
       </h2>
       <Carousel
         responsive={responsive}
         keyBoardControl
         removeArrowOnDeviceType={["desktop", "tablet", "mobile"]}
-        itemClass="px-2 rounded-lg"
+        itemClass="p-2"
       >
         {schools.map(school =>
           <School key={school.id} school={school}/>
--- components/pages/home/featured-schools-section/components/School.jsx
+++ components/pages/home/featured-schools-section/components/School.jsx
@@ -6,10 +6,10 @@ export default function School({ school }) {
   return (
     <div className="school flex flex-col items-center gap-5">
       <div className="wrapper">
-        <div className="image-cont w-[43vw] h-[37vw] sm:w-[29vw] sm:h-[23vw] lg:w-[21vw] lg:h-[15vw]">
-          <img className="image w-full h-full rounded-t" src={school.image} alt={`${school.name} Image`}/>
+        <div className="image-cont w-[41vw] h-[37vw] sm:w-[29vw] sm:h-[23vw] lg:w-[21vw] lg:h-[15vw]">
+          <img className="image w-full h-full rounded-t-lg" src={school.image} alt={`${school.name} Image`}/>
         </div>
-        <div className="name text-center p-3 rounded-b text-xs lg:text-sm bg-primaryBlue text-white">
+        <div className="name text-center p-3 rounded-b-lg text-xs lg:text-sm bg-primaryBlue text-white">
           {school.name}
         </div>
       </div>
