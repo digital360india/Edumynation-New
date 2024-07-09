89809df61e73d84ce18aeecbc14b30f4d44b5fd6
Himanshu Verma <himanshuverma544@gmail.com>
2024-07-09 10:16:20 +0530

    upd: made about-us section responsive

--- components/pages/home/about-us-section/AboutUs.jsx
+++ components/pages/home/about-us-section/AboutUs.jsx
@@ -33,18 +33,18 @@ export default function AboutUs() {
 
   return (
     <section id="about-us" className="flex flex-col gap-5 py-5">
-      <h2 className="px-5 text-3xl font-semibold text-primaryBlue">
+      <h2 className="px-5 text-3xl font-semibold text-primaryBlue lg:mx-2 xl:px-7">
         About Us
       </h2>
-      <ul className="cards flex flex-wrap justify-center px-3">
+      <ul className="cards flex flex-wrap justify-around px-3 rounded sm:mx-5 sm:bg-secondaryBlue lg:mx-7 xl:mx-9">
         {cards.map(card => 
           <Card key={card.id} card={card}/>
         )}
       </ul>
-      <p className="px-5 text-sm">
+      <p className="px-5 text-sm sm:text-center lg:mx-2 xl:px-7">
         Lorem ipsum dolor sit amet consectetur. Amet cursus mauris eget feugiat id sed. Convallis at massa eget eleifend in euismod enim non diam. Volutpat enim et rhoncus faucibus dui bibendum vivamus dictum faucibus. Gravida integer sit urna sollicitudin cursus leo neque. Rhoncus tincidunt lectus risus tortor eget. Mattis tortor porttitor urna diam sed id. Nisl nunc nulla lacus imperdiet. Mattis in nulla phasellus enim nunc ullamcorper. Sapien in aliquam id suspendisse leo aliquet in mi phasellus. Scelerisque aenean egestas proin risus. Mattis duis sed dictum ac. Ipsum elit ullamcorper odio quam faucibus. Aliquet aliquet elit lectus semper mattis proin.
       </p>
-      <button className="w-fit text-start px-10 py-1 ms-5 rounded-full bg-primaryBlue text-white">
+      <button className="w-fit px-10 py-1 ms-5 rounded-full bg-primaryBlue text-white sm:mx-auto">
         Lorem
       </button>
     </section>
--- components/pages/home/about-us-section/components/Card.jsx
+++ components/pages/home/about-us-section/components/Card.jsx
@@ -6,12 +6,12 @@ export default function Card({ card }) {
   return (
     <li 
       className={`
-        card w-[44.5%] flex flex-col items-center gap-1 m-2 p-5
+        card w-[44.5%] sm:w-fit flex flex-col items-center gap-1 p-5 m-2 sm:m-0
         bg-secondaryBlue text-primaryBlue
       `}
     >
       <Icon className="icon w-[30px] h-[30px]" icon={card.icon}/>
-      <div className="quantity mt-1 text-2xl font-semibold">
+      <div className="quantity mt-1 text-4xl font-semibold">
         {card.quantity}
       </div>
       <div className="text text-center font-semibold">
